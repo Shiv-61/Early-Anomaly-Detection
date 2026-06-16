a5a4ae327d3879749fdeb68e3cb3acd53f2bce0e
Shiv-61 <shivgowda61@gmail.com>
2026-06-17 02:12:13 +0530

    infra: docker-compose and avro schema registered

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/shiv-gowda/Projects/.venv
+set -gx VIRTUAL_ENV '/home/shiv-gowda/Anomaly Detection/.venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
